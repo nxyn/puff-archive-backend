6e246f0513759208a0d25a89e0e4875872b0f8a0
nxyn <kirthangamezyt@gmail.com>
2025-07-15 00:49:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ app.add_middleware(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://your-netlify-site.netlify.app", "http://localhost:3000"],
+    allow_origins=["https://puffarchive.netlify.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
