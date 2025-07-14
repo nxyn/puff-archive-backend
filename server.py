5c3aedad5090920c5a58e3f45e2ca7097572be6e
nxyn <kirthangamezyt@gmail.com>
2025-07-15 00:46:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -21,6 +21,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://your-netlify-site.netlify.app", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # MongoDB connection
 mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017/')
